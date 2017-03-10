b087ed07ba4f3a8867cf0890a150968bed854c42
Miles Wilson <miles.wilson@opencredo.com>
2017-03-10 10:29:18 +0000

    dns providers

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,7 +1,18 @@
 resource "aws_route53_record" "www" {
-  zone_id = "${var.route53_zone_id}"
-  name    = "${var.jenkins_name}-master.${var.route53_domain}"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.master.public_ip}"]
+  zone_id  = "${var.route53_zone_id}"
+  name     = "${var.jenkins_name}-master.${var.route53_domain}"
+  type     = "A"
+  ttl      = "300"
+  records  = ["${aws_instance.master.public_ip}"]
+  provider = "aws.dns"
+}
+
+provider "aws" {
+  alias   = "dns"
+  profile = "gghub"
+  region  = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::604083106117:role/PowerUserAccess"
+  }
 }
