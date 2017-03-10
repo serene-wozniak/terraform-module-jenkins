d0be95201195611b4b75a839b2c8226571bb6607
Miles Wilson <miles.wilson@opencredo.com>
2017-03-10 11:55:38 +0000

    [MSP-19] use private_ip to allocate

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,6 +3,6 @@ resource "aws_route53_record" "www" {
   name     = "${var.jenkins_name}-master.${var.route53_domain}"
   type     = "A"
   ttl      = "300"
-  records  = ["${aws_instance.master.public_ip}"]
+  records  = ["${aws_instance.master.private_ip}"]
   provider = "aws.dns"
 }
