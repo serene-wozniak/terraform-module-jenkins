7953fe0eaea2622531afcd60bb6085d4d2d675ee
Miles Wilson <miles.wilson@opencredo.com>
2017-03-10 11:52:27 +0000

    change security groups

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -4,31 +4,17 @@ resource "aws_security_group" "master-access" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "TCP"
-    cidr_blocks = ["${formatlist("%s/32", split(",", var.permitted_ips))}"]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
-    cidr_blocks = ["${formatlist("%s/32", split(",", var.permitted_ips))}"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "TCP"
-    cidr_blocks = ["${formatlist("%s/32", split(",", var.permitted_ips))}"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
