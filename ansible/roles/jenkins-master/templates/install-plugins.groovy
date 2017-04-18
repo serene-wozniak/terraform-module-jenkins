55c7e1a457410eba2568a24763876ce71c8223a6
Miles Wilson <miles.wilson@opencredo.com>
2017-04-18 17:12:13 +0100

    install plugins

--- ansible/roles/jenkins-master/templates/install-plugins.groovy
+++ ansible/roles/jenkins-master/templates/install-plugins.groovy
@@ -1,16 +1,27 @@
-import jenkins.model.*
+import jenkins.model.Jenkins
 import java.util.logging.Logger
 
-def logger = Logger.getLogger("")
-def installed = false
-def initialized = false
 
-def pluginParameter="{{jenkins_plugins}}"
+def logger = Logger.getLogger("")
+Boolean installed = false
+Boolean initialized = false
+
+def pluginParameter = "{{jenkins_plugins}}"
 def plugins = pluginParameter.split()
 logger.info("" + plugins)
+
+
+Jenkins.getInstance()
+
+
 def instance = Jenkins.getInstance()
 def pm = instance.getPluginManager()
 def uc = instance.getUpdateCenter()
+
+
+
+Jenkins.getInstance()
+
 uc.updateAllSites()
 
 plugins.each {
@@ -19,14 +30,18 @@ plugins.each {
     logger.info("Looking UpdateCenter for " + it)
     if (!initialized) {
       uc.updateAllSites()
+
       initialized = true
     }
     def plugin = uc.getPlugin(it)
     if (plugin) {
+
       logger.info("Installing " + it)
     	plugin.deploy()
       installed = true
     }
   }
 }
+
+
 
