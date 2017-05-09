c19e605e5d4a006497b8aadd6201257ef4eb9641
Miles Wilson <mileswilson@Miless-MacBook-Pro-3.local>
2017-05-09 11:31:16 +0100

    fix route53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,12 +7,12 @@ resource "aws_route53_record" "www" {
   provider = "aws.dns"
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "slaves" {
   zone_id  = "${var.route53_zone_id}"
   count       = "${var.slaves}"
   name     = "${var.jenkins_name}-slave-${format("%02d", count.index + 1)}.${var.route53_domain}"
   type     = "A"
   ttl      = "300"
-  records  = ["${eelement(aws_instance.slave.*.private_ip, count.index)}"]
+  records  = ["${eelement(aws_instance.slaves.*.private_ip, count.index)}"]
   provider = "aws.dns"
 }
